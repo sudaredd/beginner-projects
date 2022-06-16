ad6a4ece52a4b3a53ee3d252dd0b70fc7d134271
sudarsana kasireddy <sudaredd@gmail.com>
2022-06-16 08:08:06 -0400

    Add madlib

---
+++ madlibs.py
@@ -0,0 +1,14 @@
+# few ways to srting concatinate
+# youtuber = 'sudaredd'
+# print('You\'re subscibed to ' +youtuber)
+# print('You\'re subscibed to {}'.format (youtuber))
+# print (f'You\'re subscibed to {youtuber}')
+
+adj = input('Adjective:')
+verb1 = input('Verb1: ')
+verb2 = input('Verb2: ')
+famous_person = input('Famous person:')
+madlib = f"Computer programming is so {adj}! It makes me so excited because\
+ I love to {verb1}. Stay hydraded and {verb2} like you're {famous_person} "
+
+print(madlib)
